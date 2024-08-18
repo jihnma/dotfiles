e93c8b2c3be3f195d1fc53d154403c3e19084836
Jihnma <178634468+jihnma@users.noreply.github.com>
2024-08-18 10:04:32 +0000

    sh: introduce uninstall

---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dotfiles_dir="$HOME/.files"
+dotfiles_repository="https://github.com/jihnma/dotfiles.git"
+
+echo ""
+echo "Starting Dotfiles Uninstallation"
+
+echo "Dotfiles Uninstallation Complete"
+echo ""
