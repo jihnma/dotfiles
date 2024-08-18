4d0a7469b5179786d566951944eb06ea208f8451
Jihnma <178634468+jihnma@users.noreply.github.com>
2024-08-18 21:51:32 +0900

    sh: removed the unnecessary check

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ clone_repository() {
 }
 
 install_homebrew() {
-  if ! command -v brew &>/dev/null || [ ! -d "/opt/homebrew" ]; then
+  if ! command -v brew &>/dev/null; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     eval "$(/opt/homebrew/bin/brew shellenv)"
   fi
