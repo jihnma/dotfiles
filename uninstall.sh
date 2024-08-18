0bae495daa8e773fddfa882eb952ee64b628749c
Jihnma <178634468+jihnma@users.noreply.github.com>
2024-08-18 19:26:39 +0900

    stow: remove the symlinks

--- uninstall.sh
+++ uninstall.sh
@@ -6,5 +6,9 @@ dotfiles_repository="https://github.com/jihnma/dotfiles.git"
 echo ""
 echo "Starting Dotfiles Uninstallation"
 
+# Uninstall each package listed in the 'stowlist' file using GNU Stow
+# The '-D' flag tells Stow to remove the symlinks for each package
+cat stowlist | xargs -L1 stow -D
+
 echo "Dotfiles Uninstallation Complete"
 echo ""
